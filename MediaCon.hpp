7569a20ffffc15baafaaedc777a569eee14dfc48
ghaststeam <jumpifnotequal@yahoo.com>
2014-03-19 16:08:36 -0400

    Create MediaCon.hpp

---
+++ MediaCon.hpp
@@ -0,0 +1,38 @@
+// SCEI CXD90025G, also known as the "MediaCon", is a secondary ARM microprocessor in PS4
+// Containing the ARM v7 and v8 mnemonics
+// Threading parallelism between x86's 8-cores
+unsigned short ALUFLAGS[] =
+{
+1, // Unsigned
+2, // Zero
+3, // Carry
+4, // Overflow
+};
+const char *ThreadSync[] =
+{
+"2Thread6Thread",
+"3Thread6Thread",
+"4Thread6Thread",
+"2ThreadNoSync",
+"3ThreadNoSync",
+"4ThreadNoSync",
+};
+const char *ARMOpcodes[] =
+{
+"ADC",
+"ADD",
+"AND",
+"B",
+"BIC",
+"BL",
+"BX",
+"CDP",
+"CMN",
+"CMP",
+"EOR",
+"LDC",
+"LDM",
+"LDR",
+"MCR",
+// Will add rest with x86 opcodes
+};

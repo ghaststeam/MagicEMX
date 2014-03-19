a533dd82b8af36727565817826274bf3ffbe95a1
ghaststeam <jumpifnotequal@yahoo.com>
2014-03-19 15:41:36 -0400

    Update x86-64CORE.hpp

--- x86-64CORE.hpp
+++ x86-64CORE.hpp
@@ -1,3 +1,16 @@
+// The number of threads and synchronization between CPU cores for the processors
+const char *Threads[] =
+{
+  "4SyncMod",
+  "2SyncMod",
+  "8SyncMod",
+  "12SyncMod",
+  "14SyncMod",
+  "18SyncMod",
+  "24SyncMod",
+  "30SyncMod",
+};
+// The full list of ASCII-reference & recognized opcodes
 const char *Opcodes[] =
 {
 "AAC",
@@ -8,4 +21,5 @@ const char *Opcodes[] =
 "PUSHA",
 "POP",
 "POPA",
+// Will add the rest; operands and addressing modes compared with opcode later
 };

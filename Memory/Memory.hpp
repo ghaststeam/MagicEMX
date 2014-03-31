902bd7f6b29963f2ac4e9eb2481a41f1564084e5
Embrendassy <fudgeshaker@inbox.com>
2014-03-31 16:33:50 -0400

    Memory tricks for 8 GB

---
+++ Memory/Memory.hpp
@@ -0,0 +1,7 @@
+/* Unfinished; must include typedefs for addressing range. Larger memory tracking can be written to a file
+on disk if RAM isn't larger enough; example: mem > 2 OR 4 GB, swap memory above to file so less RAM needed.
+*/
+unsigned int RAMseg1[0x1D1A94A2000];
+unsigned int RAMseg2[0x3A352944000];
+unsigned int RAMseg3[0x574FBDE6000];
+unsigned int RAMseg4[0x746A5288000];

dd6936e53a1fee84bfe4975392fe6e712f2bb5c1
Embrendassy <fudgeshaker@inbox.com>
2014-04-29 15:35:32 -0400

    Instruction decoder & implementer for x86 opcodes

---
+++ AMDx86-64CORE/x86_Decoder.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+#include "/PS4EMX/AMDx86-64CORE/AMDx86-64.hpp"
+using namespace std;
+class FetchOpcode
+{
+public:
+FetchOpcode(short inSz, short AdrM);
+void Addressing();
+
+} FO;

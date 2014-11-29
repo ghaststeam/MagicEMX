22f9fafde25e374d741f902c47881ba272a398c9
Brenda <fudgeshaker@inbox.com>
2014-11-29 14:27:47 -0500

    commenting and cleaning

--- GPU/GCNDefines.hpp
+++ GPU/GCNDefines.hpp
@@ -1,11 +1,24 @@
 #include <GL/gl.h>
+#include <windows.h>
 #include <thread>
 #include "PS4EMX/AMDx86-64CORE/AMDx86-64.hpp"
-
+// This class is for rendering the FBO (framebuffer-object) data to the actual OpenGL context/window
+class FBOContext
+{
+ public:
+ FBOContext(); // Default constructor
+ FBOContext(HWND RHANDLE); // Constructor taking a HWND parameter for intialization of an OpenGL context
+ protected:
+ HWND RendererOut; // Handle to the renderer
+ HDC RDC; // Handle to device context
+ HGLRC HRC; // Handle to the rendering context
+ PIXELFORMATDESCRIPTOR GCNFB; // Pixel info
+};
 ///////////////////////
-bool RendererIsOn = 0;
-bool ExtResFBO = 0;
+bool RendererIsOn = 0; // Whether to emulate the graphics with an OpenGL context yet
+bool ExtResFBO = 0; // Extra Framebuffer Objects, specifically for non-PS4 graphics
 //////////////////////
+// All(most) all cache sizes, levels, channels, including texture caches
 int cachesizeL1CH1;
 int cachesizeL2CH1;
 int TexCacheC1L1;
@@ -17,21 +30,22 @@ int TexCacheC2L2;
 int TexCacheC3L2;
 int TexCacheC4L2;
 /////////////////////
-int *workitemprv = NULL;
-int *workitemgrp = NULL;
-int *SIMDops = NULL;
-int *exec_mask1 = NULL;
-int *exec_mask2 = NULL;
-int *STATUS_CODE = NULL;
-int *MODE_REGISTER_CODE = NULL;
-int base_clock = 800000000;
-short VGPRs[255]; 
-short SGPRs[103];
-short LDS[0x8000];
-uQUAD TRAP_BASE_ADDRESS;
-uQUAD TRAP_MEMORY_ADDRESS;
-uDOUBLE TRAP_STATUS;
+int *workitemprv = NULL; // Pointer to a previous work-item thread (more later for debugging, perhaps?)
+int *workitemgrp = NULL; // Pointer to a work-item group or "wavefront" for simulatenous execution threading
+int *SIMDops = NULL; // Pointer to a reference for SIMD opcode mapping
+int *exec_mask1 = NULL; // Pointer to the status of execution masks in execution of instruction flows
+int *exec_mask2 = NULL; // Pointer to even more execution masks
+int *STATUS_CODE = NULL; // Pointer to returned status codes from the GPU
+int *MODE_REGISTER_CODE = NULL; // Pointer to conditional addressing mode+register code on conditional output
+int base_clock = 800000000; // Base frequency speed for the GPU clock
+short VGPRs[255]; // Every VGPR (vector-general-purpose-register) for SIMD
+short SGPRs[103]; // Every SGPR (scalar-general-purpose-register) for SIMD
+short LDS[0x8000]; // Local data share memory per compute-unit
+uQUAD TRAP_BASE_ADDRESS; // 64-bit base address of the trap handler
+uQUAD TRAP_MEMORY_ADDRESS; // 64-bit value representing the trap handler memory range
+uDOUBLE TRAP_STATUS; // 32-bit value representing the status of the trap handler
 ////////////////////
+// This struct represents the conditional operations to be taken by scalar/vector opcodes
 struct Conditions
 {
  uBYTE SCALARS;
@@ -39,6 +53,8 @@ struct Conditions
  uBYTE MEMORY_DESCRIPTOR;
  uBYTE TRAP_EXCEPTION;
 } CONDITION;
+///////////////////
+// This struct represents the instruction flow of opcodes and related values
 struct ControlFlow
 {
  bool TERMINATE_PGM_WV;
@@ -53,21 +69,19 @@ struct ControlFlow
  uBYTE *SWAP_PC_SGPR = NULL;
  bool IGNORE_VECTOR_IN;
 } CTRL;
-struct Render_FBORes
+///////////////////
+// This struct represents capabilities for upscaling and bilinear interpolation/vertex custom-shading
+struct Render_FBOResExt
 {
 short *HQ_ModesDefine = NULL;
 short FBO_Vertex;
-
-
 } FBO_Mode;
-static short screen_width = 1920;
-static short screen_height = 1080;
-int totalPCs = 1200;
-static long int PC = 0;
+
+static short screen_width = 1920; // Internal X resolutions
+static short screen_height = 1080; // Intern Y resolutions
+int totalPCs = 1200; // All program counters and then some (for LLE)
 /////////////////////////////////////////////////////////////////////
-void SETPAIR_SGPR_PC(short &PAIRTYPE, int conditional);
-void SWAP_PCADDR_SGPR(short &addrP);
-void ColorDepthBuffer_WR(short &vertexop, void*, int microcode_es);
+
 const char *GPU_REGS[] =
 {
  // There are 256 vector registers and 104 scalar registers.
